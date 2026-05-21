e8c5495b616256a5674fd366d005c121ef759b0d
Harshakurumella <sriharsha.kurumella@sivionglobaltechnologies.com>
2026-05-21 12:59:05 +0530

    dvvhev

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(express.json());
 app.use(cors({
   origin: ['https://demo-upi.vercel.app', 'http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 app.use(helmet());
